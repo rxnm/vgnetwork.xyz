acc7c4365559d236ee63e58afeb559ab40c3469a
RyanM <Ace8104@hotmail.com>
2023-08-25 10:08:23 +0800

    Update

--- _app/gulp/config.js
+++
@@ -1,5 +0,0 @@
-var gutil = require('gulp-util');
-
-module.exports = {
-  drafts:     !!gutil.env.drafts      // pass --drafts flag to serve drafts
-};
--- _app/scripts/emailInserter.js
+++
@@ -1,7 +0,0 @@
-// Prevent spam by inserting the email link with JavaScript
-(function dynamicallyInsertEmailAddress() {
-  $(document).ready(function() {
-    var emailLink = $('#email');
-    emailLink.attr('href', 'mailto:rpmoorey@gmail.com');
-  });
-}());
